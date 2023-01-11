f00a70f9129f066cb65b3e786b834e80a2b09df3
Maxime Danic <maxime.danic@gmail.com>
2023-01-11 20:40:01 +0200

    last setting

--- app.js
+++ app.js
@@ -17,26 +17,13 @@ hamburger.addEventListener("click", () => {
 
 // valid url form //
 const inputURL = document.querySelector("#link-input");
-/*
-function isValidUrl(str) {
-  const pattern = new RegExp(
-    "^([a-zA-Z]+:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$", // fragment locator
-    "i"
-  );
-  return pattern.test(str);
-}*/
-
 function checkURL() {
   const urlValue = inputURL.value.trim();
   const error = document.querySelector("#js-error-msg");
   const shortenLink = async () => {
     const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${urlValue}`);
     const data = await res.json();
+
     return data;
   };
 
@@ -61,41 +48,31 @@ function checkURL() {
       resultLink.setAttribute("class", "shortened-link");
       sectionLink.appendChild(resultLink);
       // add html code for box for link //
-      resultLink.innerHTML = ` <a class="yourlink" href="${originalLink}"> ${originalLink} </a>
+      resultLink.innerHTML = `<div class="link-wrapper"><a class="yourlink" href="${originalLink}"> ${originalLink} </a>
       <div class="bar-link">
       <input style="opacity: 0; position: absolute; right: 1000px;" value="${shortLink}" ></input>
       <a class="ourlink" href="${shortLink}"> ${shortLink} </a>
-      <button  class="copybutton btn" type="button">Copy</button></div>`;
+      <button class="copybutton btn" type="button">Copy</button></div></div>`;
 
-      copybutton.forEach((element) => {
-        element.addEventListener("click", () => {
-          fCopy();
-          console.log("hello");
-          element.classList.add("active");
-          element.innerText = "Copied!";
+      const linkWrappers = document.querySelectorAll(".link-wrapper");
+      // copy link button //
+      linkWrappers.forEach((element) => {
+        const button = element.querySelector(".copybutton");
+        const link = element.querySelector(".ourlink");
+        function eventListener() {
+          fCopy(link);
+          button.classList.add("active");
+          button.innerText = "Copied!";
           setTimeout(() => {
-            element.classList.remove("active");
-            element.innerText = "Copy";
+            button.classList.remove("active");
+            button.innerText = "Copy";
           }, "2000");
-        });
+        }
+        button.removeEventListener("click", eventListener);
+        button.addEventListener("click", eventListener);
       });
     }
   });
-}
-
-// button copy function //
-const copybutton = document.querySelectorAll(".copybutton");
-const copyText = document.querySelectorAll(".ourlink");
-
-async function fCopy() {
-  for (element of copyText) {
-    try {
-      await navigator.clipboard.writeText(element.innerText);
-    } catch (err) {
-      console.error("Failed to copy: ", err);
-    }
-    console.log(element.innerText);
-  }
 }
 
 //button shorton it //
@@ -107,14 +84,14 @@ formShorten.addEventListener("submit", (e) => {
   checkURL();
 });
 
-/*
-function fCopy() {
-  const copybutton = document.querySelectorAll(".copybutton");
-  const copyText = document.querySelectorAll(".ourlink");
-  const shorterCopy = copyText.previousElementSibling;
-  for (element of copyText) {
-    element.innerText.select();
-    document.execCommand("copy");
-    console.log(shorterCopy);
+const copybutton = document.querySelectorAll(".copybutton");
+const copyText = document.querySelectorAll(".ourlink");
+
+async function fCopy(element) {
+  try {
+    await navigator.clipboard.writeText(element.innerText);
+  } catch (err) {
+    console.error("Failed to copy: ", err);
   }
-}*/
+  console.log(element.innerText);
+}
